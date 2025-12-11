6273552334106a03dc8f9e6dabc466275d7b7bf9
ElectronApps <barbwirek4@gmail.com>
2025-12-11 13:20:02 +0800

    Implement the most basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,147 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useMemo, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+type IngredientAmountType = "Millilitres" | "Grams" | "Pieces";
+
+type IngredientDescription = {
+  id: string;
+  name: string;
+  amountType: IngredientAmountType;
+};
+
+type Ingredient = {
+  ingredient: string;
+  amount: number;
+};
+
+type Recipe = {
+  name: string;
+  recipeSteps: string[];
+  ingredients: Ingredient[];
+};
+
+type UnknownIngredient = {
+  ingredient: string;
+  weight: number;
+};
+
+type IngredientInfo = {
+  ingredient: string;
+  has: boolean;
+};
+
+type UserRecipe = {
+  recipe: Recipe;
+  possible: boolean | null;
+};
+
+type ConnectionStep = {
+  unknownIngredients: UnknownIngredient[];
+  recipes: UserRecipe[];
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ingredients, setIngredients] = useState<IngredientDescription[]>([]);
+  const ingredientsMap = useMemo(() => {
+    const dict: { [id: string]: IngredientDescription } = {};
+    for (const ingredient of ingredients) {
+      dict[ingredient.id] = ingredient;
+    }
+    return dict;
+  }, [ingredients]);
+
+  useEffect(() => {
+    (async () => {
+      const data: IngredientDescription[] = await (
+        await fetch("/api/ingredients")
+      ).json();
+      setIngredients(data);
+    })();
+  }, []);
+
+  const [socket, setSocket] = useState<WebSocket>();
+  const [connectionStep, setConnectionStep] = useState<ConnectionStep>();
+
+  useEffect(() => {
+    const socket = new WebSocket("/ws-api/recipes");
+
+    socket.onopen = () => {
+      setSocket(socket);
+    };
+
+    socket.onmessage = (event) => {
+      setConnectionStep(JSON.parse(event.data));
+    };
+
+    return () => {
+      socket.close();
+    };
+  }, []);
+
+  const sendIngredientInfo = (info: IngredientInfo) => {
+    socket?.send(JSON.stringify(info));
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {connectionStep !== undefined && (
+        <div>
+          {connectionStep.unknownIngredients.length > 0 &&
+            connectionStep.unknownIngredients[0].weight !== 0 && (
+              <div>
+                <h1>
+                  Do you have
+                  {
+                    ingredientsMap[
+                      connectionStep.unknownIngredients[0].ingredient
+                    ]?.name
+                  }
+                  ?
+                </h1>
+                <button
+                  onClick={() =>
+                    sendIngredientInfo({
+                      ingredient:
+                        connectionStep.unknownIngredients[0].ingredient,
+                      has: true,
+                    })
+                  }
+                >
+                  Yes
+                </button>
+                <button
+                  onClick={() =>
+                    sendIngredientInfo({
+                      ingredient:
+                        connectionStep.unknownIngredients[0].ingredient,
+                      has: false,
+                    })
+                  }
+                >
+                  No
+                </button>
+              </div>
+            )}
+          <h2>Recipes:</h2>
+          {connectionStep.recipes.map((recipe) => (
+            <div>
+              {recipe.recipe.name}
+              {recipe.possible === true
+                ? "Possible"
+                : recipe.possible === false
+                ? "Not possible"
+                : "Potentially possible"}
+            </div>
+          ))}
+          {connectionStep.recipes.every(
+            (recipe) => recipe.possible === false
+          ) && "СХОДИ В МАГАЗИН ЧЕЛ"}
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
